b18ffbaf41e024c53406a91c7d5159411708fa40
Ndyy <90059029+ndyy2@users.noreply.github.com>
2025-11-24 20:21:52 +0700

    g

--- lib/models/Chat.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ChatSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  title: { type: String },
-  createdAt: { type: Date, default: Date.now },
-});
-
-export default mongoose.models.Chat || mongoose.model("Chat", ChatSchema);
